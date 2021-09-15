41478a5c6d66cebaf1c407b34d25cc60a7057007
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2021-09-16 05:03:43 +0700

    Fix intent in LoginScreen.kt and Register.kt

--- app/src/main/java/com/MasDiq/ProjectStarter/LoginScreen.kt
+++ app/src/main/java/com/MasDiq/ProjectStarter/LoginScreen.kt
@@ -2,21 +2,18 @@ package com.MasDiq.ProjectStarter
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login_screen.*
 
-class LoginScreen : AppCompatActivity(), View.OnClickListener {
+class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
-    }
 
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            btnLogin -> run {
-                val moveIntentAbout = Intent(this@LoginScreen, Register::class.java)
-                startActivity(moveIntentAbout)
-            }
+        btnRegister.setOnClickListener {
+            val move = Intent(this, Register::class.java)
+            startActivity(move)
         }
     }
+}
+
--- app/src/main/java/com/MasDiq/ProjectStarter/Register.kt
+++ app/src/main/java/com/MasDiq/ProjectStarter/Register.kt
@@ -1,12 +1,17 @@
 package com.MasDiq.ProjectStarter
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_register.*
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-
+        btnRegister.setOnClickListener {
+            val move = Intent(this, LoginScreen::class.java)
+            startActivity(move)
+        }
     }
 }
