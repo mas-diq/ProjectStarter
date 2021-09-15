8f153e90a3c3eceede8eafaddad0958bf91ce1cc
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2021-09-16 03:48:09 +0700

    Adding intent in LoginScreen.kt

--- app/src/main/java/com/MasDiq/ProjectStarter/LoginScreen.kt
+++ app/src/main/java/com/MasDiq/ProjectStarter/LoginScreen.kt
@@ -1,11 +1,22 @@
 package com.MasDiq.ProjectStarter
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login_screen.*
 
-class LoginScreen : AppCompatActivity() {
+class LoginScreen : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
     }
-}
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            btnLogin -> run {
+                val moveIntentAbout = Intent(this@LoginScreen, Register::class.java)
+                startActivity(moveIntentAbout)
+            }
+        }
+    }
