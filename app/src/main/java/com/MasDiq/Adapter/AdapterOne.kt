ac009c8999bfb9438f79ba4d6cea90c889b354d3
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2021-09-16 21:31:31 +0700

    Successful making RecyclerView

--- app/src/main/java/com/MasDiq/Adapter/AdapterOne.kt
+++ app/src/main/java/com/MasDiq/Adapter/AdapterOne.kt
@@ -1,37 +1,36 @@
 package com.MasDiq.Adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.MasDiq.Data.DataCamera
 import com.MasDiq.ProjectStarter.R
+import kotlinx.android.synthetic.main.card_layout_one.view.*
 
-class AdapterOne(private val context: Context, private val list: ArrayList<String>) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class AdapterOne(private val cameraList: ArrayList<DataCamera>) :
+    RecyclerView.Adapter<AdapterOne.ViewHolder>() {
 
-    private inner class ViewHolder internal constructor(itemView: View) :
-        RecyclerView.ViewHolder(itemView) {
-
-        var title: TextView = itemView.findViewById(R.id.tv_title)
-
-        fun bind(position: Int) {
-            title.text = list[position]
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.card_layout_one, parent, false)
+        return ViewHolder(itemView)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return ViewHolder(
-            LayoutInflater.from(context).inflate(R.layout.card_layout_one, parent, false)
-        )
-    }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as ViewHolder).bind(position)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = cameraList[position]
+        holder.image.setImageResource(item.image)
+        holder.title.text = item.title
+        holder.subtitle.text = item.subtitle
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return cameraList.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val title = itemView.tv_title!!
+        val subtitle = itemView.tv_subtitle!!
+        val image = itemView.imageCamera!!
     }
 }
---
+++ app/src/main/java/com/MasDiq/Data/DataCamera.kt
@@ -0,0 +1,3 @@
+package com.MasDiq.Data
+
+data class DataCamera(var title: String, var subtitle: String, var image: Int)
